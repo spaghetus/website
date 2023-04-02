171cdc998249417bf95f33e881a68fb90ce4226a
Willow <willow.ch@protonmail.com>
2023-04-02 15:56:26 -0500

    Fix permissions for style

--- permissions.sh
+++ permissions.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 chmod -R o-rwx wsrc
 chmod -R u+rwx wsrc
-chmod o+rx wsrc/{.,index.html,license.html,page,pages-chrono,pages-tree}
+chmod o+rx wsrc/{.,index.html,license.html,page,pages-chrono,pages-tree,style.css}
 chmod u+rx wsrc/{index.html,license.html,page,pages-chrono,pages-tree}
 chmod -R o+r wsrc/{public,page-src,Makefile,static}
 chmod +x $(find wsrc -type d)
