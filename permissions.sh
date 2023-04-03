6322c9f96145fe09fec484eefff69b3f1470dde8
Willow <willow.ch@protonmail.com>
2023-04-02 20:01:54 -0500

    made some changes to possibly allow gemini clients at some point

--- permissions.sh
+++ permissions.sh
@@ -2,6 +2,9 @@
 chmod -R o-rwx wsrc
 chmod -R u+rwx wsrc
 chmod o+rx wsrc/{.,index.html,license.html,page,pages-chrono,pages-tree,style.css}
+chmod o+rx wsrc/.res_* || true
+chmod o+rx wsrc/.logger* || true
+chmod o+rx wsrc/.gatekeeper* || true
 chmod u+rx wsrc/{index.html,license.html,page,pages-chrono,pages-tree}
 chmod -R o+r wsrc/{public,page-src,Makefile,static}
 chmod +x $(find wsrc -type d)
--- wsrc/page-src/blog/a.md
+++ wsrc/page-src/blog/a.md
@@ -1,1 +1,3 @@
 asdasdasd
+
+this is a test page with no content, please disregard
